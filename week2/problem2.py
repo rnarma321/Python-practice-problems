3f8d33a36b89cb41d06c22be77405d309a02ac73
Roshan Narma <roshan@narma.com>
2025-12-16 13:37:51 -0600

    pushed week 2

---
+++ week2/problem2.py
@@ -0,0 +1,3 @@
+# Find the smallest number where the decimal of the square root of the number starts with .357
+# Example is the square root of 5,684,736,738,957 is 2,384,268.59622757 which starts with .596
+
---
+++ week2/problem3.py
@@ -0,0 +1,4 @@
+# Download this image https://images.contentstack.io/v3/assets/bltcedd8dbd5891265b/blt5f18c2119ce26485/6668df65db90945e0caf9be6/beautiful-flowers-lotus.jpg?q=70&width=3840&auto=webp
+# rotate in 90 degrees, shrink it by 50%, and save as flower.jpg
+
+# Suggestion, you will need to run python3 -m pip install Pillow to do this, and look online for documentation
